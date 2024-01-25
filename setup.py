211e48a079400392aa32f302f47d1f79a2d8cb45
NirbhaySedha <Sedha9nirbhay@gmail.com>
2024-01-25 14:46:54 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project contains the dvc and mlflow practices',
+    author='Nirbhay Sedha',
+    license='MIT',
+)
